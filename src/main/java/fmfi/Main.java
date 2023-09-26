0bb5d5dff63e9b703ebf11211866b88379f2054b
Anton Kica <realantonkica@gmail.com>
2023-09-26 11:08:42 +0200

    Added timeouts and reordered checking

--- src/main/java/fmfi/Main.java
+++ src/main/java/fmfi/Main.java
@@ -7,12 +7,14 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,6 +24,9 @@ import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import java.security.SecureRandom;
 import java.sql.*;
+import java.text.MessageFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class Main extends ListenerAdapter {
@@ -40,7 +45,7 @@ public class Main extends ListenerAdapter {
 
     public static void main(String[] args) throws InterruptedException, SQLException {
         mailer = new UnibaMailer();
-        
+
         final String jdbcUrl = System.getProperty("jdbc.url");
         connection = DriverManager.getConnection(jdbcUrl);
         connection.setAutoCommit(true);
@@ -71,6 +76,7 @@ public class Main extends ListenerAdapter {
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event)
     {
         event.deferReply(true).queue();
+        InteractionHook hook = event.getHook();
         // make sure we handle the right command
         switch (event.getName()) {
             case CommandAuth -> {
@@ -81,7 +87,7 @@ public class Main extends ListenerAdapter {
 
                 if (!acceptedRoles.contains(role.getName())) {
                     logger.info("User {} requested invalid role {}", discordName, role);
-                    event.getHook().sendMessage("Requested role is not permitted, request following roles: " + String.join(", ", acceptedRoles)).setEphemeral(true).queue();
+                    hook.sendMessage("Requested role is not permitted, request following roles: " + String.join(", ", acceptedRoles)).setEphemeral(true).queue();
                     break;
                 }
 
@@ -96,7 +102,13 @@ public class Main extends ListenerAdapter {
                     break;
                 }
 
-                // todo verify repeated requests for Discord user and ais user as well
+                if(hasExistingRequest(discordName, hook)) {
+                    break;
+                }
+
+                if(checkForTimeout(discordName, hook) || checkForTimeout(aisName, hook))
+                    break;
+
 
                 String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
                 var random = new SecureRandom();
@@ -122,6 +134,7 @@ public class Main extends ListenerAdapter {
                     event.getHook().sendMessage("Failed to send mail to your account").setEphemeral(true).queue();
                     break;
                 }
+                logger.info("Verification request for account {} has been sent to {}.", discordName, aisName);
                 event.getHook().sendMessage("Your random code has been sent your university email").setEphemeral(true).queue();
             }
             case CommandConf -> {
@@ -140,7 +153,6 @@ public class Main extends ListenerAdapter {
                         event.getHook().sendMessage("Invalid verification code").setEphemeral(true).queue();
                         break;
                     }
-
                     String roleId = resultSet.getString("roleId");
 
                     resultSet.close();
@@ -152,6 +164,14 @@ public class Main extends ListenerAdapter {
                     guild.addRoleToMember(discordUser, role).queue();
 
                     event.getHook().sendMessage("Your role has been set to " + role.getName()).setEphemeral(true).queue();
+
+
+                    query = "delete from VerificationRequest where discordName = ? and code = ?";
+                    preparedStatement = connection.prepareStatement(query);
+                    preparedStatement.setString(1, discordName);
+                    preparedStatement.setString(2, code);
+                    preparedStatement.execute();
+                    preparedStatement.close();
                 } catch (SQLException e) {
                     throw new RuntimeException(e);
                 }
@@ -161,5 +181,97 @@ public class Main extends ListenerAdapter {
 
             }
         }
+    }
+
+    /**
+     * May be extended to check for AIS name as well
+     * @param aisName
+     * @param discordName
+     * @param hook
+     * @return
+     */
+    private boolean hasExistingRequest(String discordName, InteractionHook hook) {
+        try{
+            String query = "select 1 from VerificationRequest where discordName = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, discordName);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if(resultSet.next()) {
+                hook.sendMessage("There's already a pending request for your discord account, check your email.").queue();
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (SQLException e) {
+            logger.error("Exception in checking for existing request.", e);
+            hook.sendMessage("There was an error while processing your request.").queue();
+            return true;
+        }
+    }
+
+    private boolean checkForTimeout(String name, InteractionHook hook) {
+        try {
+            String query = "select suspendedFrom, suspendedCount from AccountTimeout where accountName = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, name);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            int suspendedCount;
+            LocalDateTime suspendedFrom;
+            if(!resultSet.next()) {
+                suspendedCount = 0;
+                suspendedFrom = LocalDateTime.now();
+                query = "insert into AccountTimeout(suspendedFrom, suspendedCount, accountName) values(?,?,?)";
+            } else {
+                suspendedCount = Math.min(resultSet.getInt("suspendedCount") + 1, 4);
+                suspendedFrom = resultSet.getTimestamp("suspendedFrom").toLocalDateTime();
+                query = "update AccountTimeout set suspendedFrom = ?, suspendedCount = ? where accountName = ?";
+            }
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
+            preparedStatement.setInt(2, suspendedCount);
+            preparedStatement.setString(3, name);
+            preparedStatement.execute();
+            preparedStatement.close();
+
+            LocalDateTime suspendedUntil = getSuspendedTime(suspendedFrom, suspendedCount);
+            if(suspendedUntil.isAfter(LocalDateTime.now())) {
+                String suspendedUntilFormatted = suspendedUntil.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                String message = MessageFormat.format("The account {0} is suspended until {1}.", name, suspendedUntilFormatted);
+                hook.sendMessage(message).queue();
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SQLException e) {
+            logger.error("Exception in checking for timeout.", e);
+            hook.sendMessage("There was an error while processing your request.").queue();
+            return true;
+        }
+    }
+
+    @NotNull
+    private static LocalDateTime getSuspendedTime(LocalDateTime suspendedFrom, int suspendedCount) {
+        LocalDateTime suspendedUntil;
+        switch (suspendedCount) {
+            case 0 -> {
+                suspendedUntil = LocalDateTime.MIN;
+            }
+            case 1 -> {
+                suspendedUntil = suspendedFrom.plusMinutes(5);
+            }
+            case 2 -> {
+                suspendedUntil = suspendedFrom.plusHours(1);
+            }
+            case 3 -> {
+                suspendedUntil = suspendedFrom.plusDays(1);
+            }
+            default -> {
+                suspendedUntil = LocalDateTime.MAX;
+            }
+        }
+        return suspendedUntil;
     }
 }
