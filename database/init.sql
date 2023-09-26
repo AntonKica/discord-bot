f6022a2131ffac7fda54820553120ee34bf0bc2c
Anton Kica <realantonkica@gmail.com>
2023-09-26 11:43:18 +0200

    Init scripts now clears previous tables

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,13 @@
+drop table VerificationRequest;
+drop table AccountTimeout;
+
 create table VerificationRequest(
     discordName varchar(120) primary key,
     roleId char(12),
     code char(60)
+);
+create table AccountTimeout(
+    accountName varchar(120) primary key,
+    suspendedFrom datetime,
+    suspendedCount int
 );
